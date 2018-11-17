998421903f248ec5d8ff1a3e494ad041cdfea1b5
Arhell <arhell333@gmail.com>
2018-11-17 19:39:46 +0200

    add gulp gulp-autoprefixer & test

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,13 @@
 var gulp = require('gulp'),
-    sass = require('gulp-sass');
+    sass = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('sass', function () {
   return gulp.src('scss/**/*.scss')
     .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer({
+      browsers: ['last 10 versions'],
+      cascade: false
+    }))
     .pipe(gulp.dest('./css'));
 });
