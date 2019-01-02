352860067ece416d90396e761ff26c509a656d91
Arhell <arhell333@gmail.com>
2019-01-02 21:13:56 +0200

    add index.pug

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp'),
     pug = require('gulp-pug')
 
 gulp.task('pug'), function () {
-  return gulp.src('src/pug/*.pug')
+  return gulp.src('src/index.pug')
     .pipe(pug({
       pretty: true
     }))
