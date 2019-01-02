aeb857b9d0c49c680f25397181daf271f5e31609
Arhell <arhell333@gmail.com>
2019-01-03 00:53:47 +0200

    test pug task

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,10 @@
 var gulp = require('gulp'),
     pug = require('gulp-pug')
 
-gulp.task('pug'), function () {
+gulp.task('pug', function () {
   return gulp.src('src/index.pug')
     .pipe(pug({
       pretty: true
     }))
-    .pipe(gulp.dest('/'));
-}
+    .pipe(gulp.dest('./'));
+})
